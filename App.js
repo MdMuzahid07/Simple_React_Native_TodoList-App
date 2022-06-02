4dd7f51c86072e79b681fa154a9796da93705677
MdMuzahid07 <MdMuzahid7396@gmail.com>
2022-06-02 18:41:43 +0600

    basic design complete

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionText}>Today's tasks</Text>
+        <View style={styles.items}>
+          <Task  text={'task 1'}/>
+          <Task  text={'task 2'}/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'grey',
   },
+  taskWrapper: {
+    paddingTop: 70,
+    paddingHorizontal: 20,
+  },
+  sectionText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+
+  }
 });
---
+++ components/Task.js
@@ -0,0 +1,47 @@
+import { TouchableOpacity, StyleSheet, View, Text } from 'react-native'
+import React from 'react'
+
+export default function Task({ text }) {
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <View style={styles.square}></View>
+                <Text style={styles.itemText}>{text}</Text>
+            </View>
+            <View style={styles.circular}></View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+        marginTop: 20,
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: 'purple',
+        borderRadius: 5,
+        marginRight: 15
+    },
+    circular: {
+        width: 12,
+        height: 12,
+        borderColor: 'red',
+        borderWidth: 2,
+        borderRadius: 5
+
+    }
+})
